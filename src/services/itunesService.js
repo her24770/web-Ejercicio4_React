fdc51f9fa7213ced5f4511fb1e6033def487eb10
her24770 <her24770@uvg.edu.gt>
2026-04-29 11:56:15 -0600

    llamada iniciales de api Itunes para extraccion de musica

---
+++ src/services/itunesService.js
@@ -0,0 +1,25 @@
+const BASE_URL = 'https://itunes.apple.com'
+
+export async function getTopSongs() {
+  const res = await fetch(`${BASE_URL}/us/rss/topsongs/limit=20/json`)
+  const data = await res.json()
+  return data.feed.entry
+}
+
+export async function searchMusic(query) {
+  const res = await fetch(`${BASE_URL}/search?term=${encodeURIComponent(query)}&media=music&limit=25`)
+  const data = await res.json()
+  return data.results
+}
+
+export async function getArtistDetail(artistId) {
+  const res = await fetch(`${BASE_URL}/lookup?id=${artistId}&entity=song&limit=10`)
+  const data = await res.json()
+  return data.results
+}
+
+export async function getArtistAlbums(artistId) {
+  const res = await fetch(`${BASE_URL}/lookup?id=${artistId}&entity=album&limit=10`)
+  const data = await res.json()
+  return data.results
+}
